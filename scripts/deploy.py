05508303729cfeabf4f7c5c5c72814739f5dcee1
“Satwik <ep20btech11027@iith.ac.in>
2022-12-27 00:07:37 +0530

    Deploy Ready

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,19 +2,25 @@ from scripts.helpful_scripts import get_account
 
 from brownie import Issue, Opening, Params, Request, Verify, G, BN256G2, network, config
 
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from web3.middleware import construct_sign_and_send_raw_middleware
+
+from web3 import Web3
+
 
 def deploy():
 
     account = get_account()
-    #G_deploy = G.deploy({"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
-    #BN256G2_deploy = BN256G2.deploy({"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
+    G_deploy = G.deploy({"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
+    BN256G2_deploy = BN256G2.deploy({"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
     
-    #Params_deploy = Params.deploy({"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"])'''
-    #print(Params_deploy)
-    #Issue_deploy = Issue.deploy("0x8aE516da91de4A38C00e5309089A70Cd45CD916b", {"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
-    #Opening_deploy = Opening.deploy("0x8aE516da91de4A38C00e5309089A70Cd45CD916b", {"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
-    #Request_deploy = Request.deploy("0x8aE516da91de4A38C00e5309089A70Cd45CD916b", {"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
-    #Verify_deploy = Verify.deploy("0x8aE516da91de4A38C00e5309089A70Cd45CD916b", {"from": get_account(1)})#""", publish_source = config["networks"][network.show_active()]["verify"]"""
+    Params_deploy = Params.deploy({"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"])'''
 
+    Issue_deploy = Issue.deploy(Params_deploy, {"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
+    Opening_deploy = Opening.deploy(Params_deploy, {"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
+    Request_deploy = Request.deploy(Params_deploy, {"from": account})#''', publish_source = config["networks"][network.show_active()]["verify"]'''
+    Verify_deploy = Verify.deploy(Params_deploy, {"from": get_account(1)})#""", publish_source = config["networks"][network.show_active()]["verify"]"""
+    
 def main():
     deploy()
